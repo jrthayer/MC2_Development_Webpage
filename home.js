4ca637c45c3904bfd9f46eeb26da5ef306875d4e
James Thayer <jrthayer@users.noreply.github.com>
2019-05-05 11:09:22 -0700

    Add Finishing Touches
    *add Lato 300 weight font files
    *adjust the padding of the info section for standard and mobile viewing

--- home.js
+++ home.js
@@ -1,9 +1,12 @@
 var inputs = document.getElementsByTagName("input");
 
-var name = inputs[0];
+var username = inputs[0];
 var email = inputs[1];
 var number = inputs[2];
 
-name.pattern = "[a-zA-Z]*";
+username.pattern = "[a-zA-Z ]*";
 email.pattern = "([a-zA-Z0-9\.]*)(@)([a-zA-Z]*)(\.)([a-zA-Z]*)";
 number.pattern = "([0-9/(/)-]*)";
+
+username.required = true;
+email.required = true;
