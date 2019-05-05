be216aadefca1450c777dee1eea78d3ee56db977
James Thayer <jrthayer@users.noreply.github.com>
2019-05-05 09:57:36 -0700

    Set Input Patterns
    *name can only be letters
    *email is:
            -letters, number, and periods followed by an @ symbol
            -then any letters followed by a /.
             -ending with any letters
    *number is restricted to 0-9, (, ), and -

--- home.js
+++ home.js
@@ -4,4 +4,6 @@ var name = inputs[0];
 var email = inputs[1];
 var number = inputs[2];
 
-console.log(inputs);
+name.pattern = "[a-zA-Z]*";
+email.pattern = "([a-zA-Z0-9\.]*)(@)([a-zA-Z]*)(\.)([a-zA-Z]*)";
+number.pattern = "([0-9/(/)-]*)";
